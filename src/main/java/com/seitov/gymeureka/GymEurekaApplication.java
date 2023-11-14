797fe97bf1d528bc0390fdba7852233174b596f6
KIBI312 <isken00.kg@gmail.com>
2023-11-14 13:28:28 +0600

    Initial commit

---
+++ src/main/java/com/seitov/gymeureka/GymEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.seitov.gymeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymEurekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seitov/gymeureka/GymEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seitov.gymeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
